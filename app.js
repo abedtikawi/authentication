d6d1f0c606b7f366b5458cf03fb581847e979a5a
AbedTikkawi <abed.tikkawi@360mea.com>
2022-05-12 16:55:58 +0300

    added cors and helmet

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // libraries
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 // files
 const connectDB = require('./db/DBConnection');
 const indexRouter = require('./routes');
 
 const app = express();
 
+//db connections
 connectDB();
+
+// security
+app.use(helmet());
+app.use(cors());
+app.use(helmet.hidePoweredBy());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
