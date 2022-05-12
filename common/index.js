d35b1d385921054c86033e35189a4cf7f1ace7af
AbedTikkawi <abed.tikkawi@360mea.com>
2022-05-12 16:17:16 +0300

    added email types for centralizing flow

--- common/index.js
+++ common/index.js
@@ -2,7 +2,13 @@ const VALIDATOR_NAMES = {
   SIGNUP: 'signup',
   SIGNIN: 'signin',
 };
+const EMAIL_TYPES = {
+  SIGNUP: 'signup',
+  SIGNIN: 'signin',
+  LOCKED: 'locked',
+};
 
 module.exports = {
   VALIDATOR_NAMES,
+  EMAIL_TYPES
 };
