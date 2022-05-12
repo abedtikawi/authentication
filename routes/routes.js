bb6fbd3037e2ec9cb9bdc7e50c4faa2f36e3f753
AbedTikkawi <abed.tikkawi@360mea.com>
2022-05-12 16:18:45 +0300

    renamed validation handler

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 const { VALIDATOR_NAMES } = require('../common/index');
-const authMiddleware = require('../middlewares/authentication');
+const handler = require('../middlewares/authentication');
 
 const userControllers = {
   signup: {
     controller: require('../controllers/users/signup'),
-    handler: authMiddleware(VALIDATOR_NAMES.SIGNUP),
+    handler: handler(VALIDATOR_NAMES.SIGNUP),
   },
   signin: {
     controller: require('../controllers/users/signin'),
-    handler: authMiddleware(VALIDATOR_NAMES.SIGNIN),
+    handler: handler(VALIDATOR_NAMES.SIGNIN),
   },
 };
 
